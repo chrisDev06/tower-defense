2794ead1a6d4a79015b37b90c8e0728ce63f890e
chrisDev06 <christophermassondev.epitech@gmail.com>
2023-10-29 03:40:44 +0100

    #6 | Sprite array

--- main/GameScreen.java
+++ main/GameScreen.java
@@ -3,6 +3,7 @@ package main;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.JPanel;
 
@@ -11,30 +12,41 @@ public class GameScreen extends JPanel {
     private Random random;
     private BufferedImage img;
 
+    private ArrayList<BufferedImage> sprites = new ArrayList<>();
+
     public GameScreen(BufferedImage img) {
         this.img = img;
+
+        loadSprites();
         random = new Random();
+    }
+
+    public void loadSprites() {
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                sprites.add(img.getSubimage(x * 32, y * 32, 32, 32));
+            }
+        }
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawImage(img, 0, 0, null);
+        g.drawImage(sprites.get(20), 0, 0, null);
 
-        // for (int y = 0; y < 20; y++) {
-        //     for (int x = 0; x < 20; x++) {
+        // Exemple drawing img 320px
+        // g.drawImage(img.getSubimage(32 * 9, 32, 32, 32), 0, 0, null);
+        // --------------------------
 
-        //         g.setColor(getRndColor());
-        //         g.fillRect(x * 32, y * 32, 32, 32);
-        //     }
-        // }
+        for (int y = 0; y < 20; y++) {
+            for (int x = 0; x < 20; x++) {
+                g.drawImage(sprites.get(getRndInt()), x * 32, y * 32, null);
+            }
+        }
     }
 
-    // private Color getRndColor() {
-    //     int r = random.nextInt(256);
-    //     int g = random.nextInt(256);
-    //     int b = random.nextInt(256);
+    private int getRndInt() {
+        return random.nextInt(100);
 
-    //     return new Color(r, g, b);
-    // }
+    }
 }
